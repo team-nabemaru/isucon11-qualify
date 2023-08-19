fb00f2b4b1cde6fd7f7646ff72c68b6135b5606f
6rawn <103933406+6rawn@users.noreply.github.com>
2023-08-19 16:51:41 +0900

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 
 cd /home/isucon/webapp/go/
 go build -o isucondition
-cp development/nginx-backend/isucondition.conf /etc/nginx/sites-enabled
+cp /home/isucon11-qualify/development/nginx-backend/isucondition.conf /etc/nginx/sites-enabled
 
 systemctl restart isucondition.go.service
 systemctl restart jiaapi-mock.service
