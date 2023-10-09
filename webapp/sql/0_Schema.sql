7abf14f40201a710f5a96ffadc8885921bcd006f
nakayan5 <tomoya.progress@gmail.com>
2023-10-09 19:14:59 +0900

    fix: sql syntax

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
+  PRIMARY KEY(`id`),
   INDEX idx_character (`character`),
   INDEX idx_jiauserid (`jia_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
@@ -29,7 +29,7 @@ CREATE TABLE `isu_condition` (
   `score` tinyint AS (CASE is_dirty + is_overweight + is_broken WHEN 3 THEN 3 WHEN 0 THEN 0 ELSE 1 END) STORED,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
+  PRIMARY KEY(`id`),
   INDEX idx_jiaisuuuid_timestamp (`jia_isu_uuid`, `timestamp`),
   INDEX idx_jiaisuuuid (`jia_isu_uuid`),
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
